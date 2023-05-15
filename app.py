7e8db3b82aca721f7c69ee8a96714b71116525e3
tak59 <dev.hoyte@gmail.com>
2023-05-15 01:38:56 -0400

    added env

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Whats up")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "What's Up?"
+
+print(__name__)
+if __name__ = "__main__":
+	app.run(host='0.0.0.0', debug=True)
