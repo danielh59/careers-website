c9db48b5f69aa9901346912375d2d0de20a2634b
tak59 <dev.hoyte@gmail.com>
2023-05-18 00:11:03 -0400

    added dynamic data to homepage. more styling

--- app.py
+++ app.py
@@ -2,9 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+      {
+            'id': 1,
+            'title': 'Data Analyst',
+            'location': 'New York,New York',
+            'salary': '$210,000'
+      },
+      {
+            'id': 2,
+            'title': 'Data Scientist',
+            'location': 'Miami, Florida',
+            'salary': '$240,000'
+      },
+      {
+            'id': 3,
+            'title': 'Backend Engineer',
+            'location': 'Malibu,California',
+            'salary': '$130,000'
+      },
+       {
+            'id': 4,
+            'title': 'Frontend Developer',
+            'location': 'Remote',
+            'salary': '$120,000'
+      }
+      
+]
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='OneTech')
+
 @app.route("/listings")
 def index_page():
       return render_template('index.html')
